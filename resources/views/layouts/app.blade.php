fcdfb45fe0e802812d910bdfff84399563d5dda0
kingdaniel101 <kingdaniel101@ikooba.com>
2020-06-12 11:39:11 +0100

    commitChange

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-    <meta charset="utf-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" href="{{asset('/css/app.css')}}">
 	<link rel="stylesheet" href="{{asset('css/vue.css')}}">
+	
 	</head>
 	<body>
 	<div id="app">
@@ -25,7 +27,7 @@
 
 
 
-
+ 
 
 
 
